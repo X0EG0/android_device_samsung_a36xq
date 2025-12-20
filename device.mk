0c4b19555a1258076772c6f0fab657d94c4389da
CatDev3 <168304418+DevCat3@users.noreply.github.com>
2025-12-20 05:06:35 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,7 +10,7 @@ LOCAL_PATH := device/samsung/a36xq
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
