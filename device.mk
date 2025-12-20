0997fbf6b0feab00fadbbdd449d37e915a4940fa
CatDev3 <168304418+DevCat3@users.noreply.github.com>
2025-12-20 07:10:39 +0200

    Restore update_engine_sideload in device.mk
    Re-add update_engine_sideload to PRODUCT_PACKAGES.

--- device.mk
+++ device.mk
@@ -33,7 +33,3 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
-    
-PRODUCT_PACKAGES += \   
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
