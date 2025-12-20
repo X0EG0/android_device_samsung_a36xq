8a31a59c5d869cccef3a862db8ab6bf96e220079
CatDev3 <168304418+DevCat3@users.noreply.github.com>
2025-12-20 05:49:10 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,12 +14,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+#PRODUCT_PACKAGES += \
+#    android.hardware.boot@1.0-impl \
+#    android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.parrot
+#PRODUCT_PACKAGES += \
+#    bootctrl.parrot
 
 #PRODUCT_STATIC_BOOT_CONTROL_HAL := \
 #    bootctrl.parrot \
